8787ab2663ee2be416d89d004415f278576fb970
xeangit <mily072@naver.com>
2022-11-17 16:25:51 +0900

    Initial commit

---
+++ src/main/java/com/example/unifesta/UnifestaApplication.java
@@ -0,0 +1,13 @@
+package com.example.unifesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifestaApplication.class, args);
+	}
+
+}
